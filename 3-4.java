e27f786a64522d59cf40303a39346f9d2ac11643
HanSolo0219 <59414706+HanSolo0219@users.noreply.github.com>
2022-03-31 14:58:02 +0800

    Update 3-4.java

--- 3-4.java
+++ 3-4.java
@@ -1,24 +1,20 @@
 package com.hdfsclient;
+
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.FileSystems;
-
-import javax.naming.ConfigurationException;
-import javax.tools.StandardJavaFileManager.PathFactory;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
-public class CreateDir{
+public class CreateDir {
     public static void main(String[] args){
         String uri = "hdfs;//master:9000/user/test";
         Configuration conf = new Configuration();
-        try{
-            FileSystem fs =FileSystem.get(URI.create(uri),conf);
-            PathFactory dfs =new Path("hdfs://master:9000/user/test");
+        try {
+            FileSystem fs =FileSystem.get(URI.create(uri), conf);
+            Path dfs =new Path("hdfs://master:9000/user/test");
             fs.mkdirs(dfs);
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }

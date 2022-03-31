72c009d42d17cd005680dedd0ad2c0962ae0ea5c
HanSolo0219 <59414706+HanSolo0219@users.noreply.github.com>
2022-03-31 16:38:01 +0800

    Update 3-2.java

--- 3-2.java
+++ 3-2.java
@@ -9,15 +9,15 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
  
 public class FileSystemCat {
-    public static void main(String[]  args) throws IOException{
-        String uri ="hdfs://master:9000/user/hadoop/test";
-        Configuration conf = new Configuration() ;
+    public static void main(String[]  args) throws IOException {
+        String uri = "hdfs://master:9000/user/hadoop/test";
+        Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(URI.create(uri), conf);
    
         InputStream in = null;
         try {
             in = fs.open(new Path(uri));
-            IOUtils.copyBytes(in,System.out,4096,false);
+            IOUtils.copyBytes(in, System.out, 4096,false);
         }   finally{
             IOUtils.closeStream(in);
         }

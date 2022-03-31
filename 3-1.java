f6cd27fd7a7ade84c94a2fb5293c4a73affb0c81
HanSolo0219 <59414706+HanSolo0219@users.noreply.github.com>
2022-03-31 15:07:28 +0800

    Update 3-1.java

--- 3-1.java
+++ 3-1.java
@@ -1,25 +1,25 @@
 package com.hdfsclient;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.net.URL;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.path;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.apache.hadoop.io.IOUtils;
  
-public class FileSystemCat {
-    public static void main(String[]  args) throws IOException{
-        String uri ="hdfs://master:9000/user//hadoop/test";
-        Configuration conf = new Configuration() ;
-        FileSystem fs =FileSystem.get(URL.create(url),conf);
-   
+public class MyCat {
+
+     static{
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main (String[] args) throws MalformedURLException, IOException{
         InputStream in = null;
         try {
-            in =fs.open(new path(uri));
-            IOUtils.copyBytes(in,System.out,4096,false);
-        }   finally{
-            IOUtils.closeStream(in);
-        }
-    
-    }   
+         in =new URL(args[0]).openStream();
+         IOUtils.copyBytes(in, System.out, 4096,false);
+    } finally{
+         IOUtils.closeStream(in);
+        }   
+    }                   
 }

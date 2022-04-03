4513aa0288c166db271e507edd2084b05b5b5998
HanSolo0219 <59414706+HanSolo0219@users.noreply.github.com>
2022-04-03 18:25:55 +0800

    Update 3-4.java

--- 3-4.java
+++ 3-4.java
@@ -7,11 +7,12 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class CreateDir {
+    
     public static void main(String[] args){
-        String uri = "hdfs;//master:9000/user/test";
+        String uri = "hdfs：//master:9000/user/test";
         Configuration conf = new Configuration();
         try {
-            FileSystem fs =FileSystem.get(URI.create(uri), conf);
+            FileSystem fs = FileSystem.get(URI.create(uri), conf);
             Path dfs =new Path("hdfs://master:9000/user/test");
             fs.mkdirs(dfs);
         } catch (IOException e) {

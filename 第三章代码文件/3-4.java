f5d63f7494e32d8521a895f3cb833824e68fc187
HanSolo0219 <59414706+HanSolo0219@users.noreply.github.com>
2022-04-15 14:25:24 +0800

    Add files via upload

---
+++ 第三章代码文件/3-4.java
@@ -0,0 +1,22 @@
+package com.hdfsclient;
+
+import java.io.IOException;
+import java.net.URI;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class CreateDir {
+    
+    public static void main(String[] args){
+        String uri = "hdfs：//master:9000/user/test";
+        Configuration conf = new Configuration();
+        try {
+            FileSystem fs = FileSystem.get(URI.create(uri), conf);
+            Path dfs =new Path("hdfs://master:9000/user/test");
+            fs.mkdirs(dfs);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
